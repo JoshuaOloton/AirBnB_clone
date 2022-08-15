7c4fdcce328bec2eb2d711863f9ccb5c1a4d9c55
The-Great1 <54747205+The-Great1@users.noreply.github.com>
2022-08-15 13:52:29 +0100

    Added Create command

--- console.py
+++ console.py
@@ -3,6 +3,16 @@
 
 import cmd
 from models import storage
+import shlex
+import re
+import ast
+from models.base_model import BaseModel
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.user import User
+from models.state import State
 
 
 class HBNBCommand(cmd.Cmd):
@@ -37,6 +47,33 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, arg):
         """ EOF command to exit the program\n"""
         return True
+    
+    # program commands
+    def do_create(self, line):
+        """Create new instance of class mode\n"""
+        if not line:
+            print("**class name missing**")
+            return False
+        args = line.split()
+        if args[0] == "BaseModel":
+            model = BaseModel()
+        elif args[0] == "User":
+            model = User()
+        elif args[0] == "Amenity":
+            model = Amenity()
+        elif args[0] == "City":
+            model = City()
+        elif args[0] == "Place":
+            model = Place()
+        elif args[0] == "Review":
+            model = Review()
+        elif args[0] == "State":
+            model = State()
+        else:
+            print("** class doesn't exist **")
+            return False
+        print(model.id)
+        model.save()
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
