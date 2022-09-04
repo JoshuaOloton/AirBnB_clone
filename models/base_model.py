f629a5be57855fcf53aa099b1bc4c15669e89851
Joshua Oloton <olotonjoshua@gmail.com>
2022-09-04 09:26:27 +0100

    fixed circular import issue

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@
 
 import uuid
 from datetime import datetime
-from . import storage
+import models
 
 
 class BaseModel:
@@ -15,7 +15,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
         else:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
@@ -28,7 +28,7 @@ class BaseModel:
     def save(self):
         """ updates public attribute updated_at with current datetime """
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """ returns dictionary with keys/values of __dict__ of the instance """
